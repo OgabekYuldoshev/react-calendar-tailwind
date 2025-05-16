d8e9a5e879c9c53183d0a47d079ae94e1cf50234
OgabekYuldoshev <yuldashoff1@gmail.com>
2025-05-16 23:56:08 +0500

    style: make some styling in timeline
    \

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import { Calendar } from "./components/calendar";
 import { Navbar } from "./components/navbar";
 
 export default function App() {
-  const [tab, setTab] = useState("timeline");
+  const [tab, setTab] = useState("Timeline");
   return (
     <main className="w-full min-h-screen flex flex-col relative">
-      <Navbar tabs={["calendar", "timeline"]} {...{ tab, setTab }} />
+      <Navbar tabs={["Calendar", "Timeline"]} {...{ tab, setTab }} />
       <section className="p-4">
         {
           {
-            timeline: <Timeline />,
-            calendar: <Calendar />,
+            Timeline: <Timeline />,
+            Calendar: <Calendar />,
           }[tab]
         }
       </section>
