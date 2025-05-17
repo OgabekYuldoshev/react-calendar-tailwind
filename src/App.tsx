dbf943b6489d947fdd691737bbd5424e8540a11c
OgabekYuldoshev <yuldashoff1@gmail.com>
2025-05-17 17:29:03 +0500

    feat: calendar component is done

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { Calendar } from "./components/calendar";
 import { Navbar } from "./components/navbar";
 
 export default function App() {
-  const [tab, setTab] = useState("Timeline");
+  const [tab, setTab] = useState("Calendar");
   return (
     <main className="w-full min-h-screen flex flex-col relative">
       <Navbar tabs={["Calendar", "Timeline"]} {...{ tab, setTab }} />
-      <section className="p-4">
+      <section className="p-4 flex-1 flex flex-col">
         {
           {
             Timeline: <Timeline />,
